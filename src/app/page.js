bbf2bcfe36ca61833e4642ae098334da36cfede2
tushar <tushar.kamlaai@gmail.com>
2025-01-26 08:57:19 +0530

    'addfadenew.

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,19 @@ import LearningJourney from './components/ui/LearningJourney'
 import Map from '@/app/components/ui/Map'
 import OurTraining from './components/ui/OurTraining'
 import OnlineTraining from './components/ui/OnlineTraining'
+import { Fade } from "react-awesome-reveal";
+
 const Home = () => {
   return (
     <>
-      <Carousel />
-      <Card />
-      <LearningJourney />
-      <OurTraining />
-      <Map />
-      <OnlineTraining/>
+      <Fade>
+        <Carousel />
+        <Card />
+        <LearningJourney />
+        <OurTraining />
+        <Map />
+        <OnlineTraining />
+      </Fade>
     </>
   )
 }
